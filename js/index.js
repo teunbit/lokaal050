ed97f446ae334d0129e477d03504b4687985fc39
Jean-Marc Koopmans <jkoopmans1@st.noorderpoort.nl>
2023-09-13 12:55:39 +0200

    hero geupdated

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const hero_1 = document.getElementById("hero_1");
+const text_1 = document.getElementById("toggle_text_1");
+
+hero_1.addEventListener("click", () => {
+    // text_1.classList.toggle("hidden");
+    text_1.classList.toggle("h-screen");
+})
