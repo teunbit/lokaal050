fe06bed24a624e0039042ffdc5f09ef4ab76f69a
Jean-Marc Koopmans <jkoopmans1@st.noorderpoort.nl>
2023-09-13 14:24:37 +0200

    hero geupdated

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
-const hero_1 = document.getElementById("hero_1");
-const text_1 = document.getElementById("toggle_text_1");
+const heroes = document.querySelectorAll(".heroes>div");
 
-hero_1.addEventListener("click", () => {
-    // text_1.classList.toggle("hidden");
-    text_1.classList.toggle("h-screen");
-})
+heroes.forEach(hero => {
+    const button = hero.querySelector("a");
+    const collapseable_text = hero.querySelector("div>div>div");
+    button.addEventListener("click", (e) => {
+        e.preventDefault();
+        collapseable_text.classList.toggle("h-screen");
+        collapseable_text.classList.toggle("mb-0");
+    })
+});
