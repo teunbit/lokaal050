35625a13b99c86820e025725ad94c4e63e2fe9be
Teun Kluiving <thkluiving@st.noorderpoort.nl>
2023-09-14 13:20:10 +0200

    index merged

--- js/twconfig.js
+++ js/twconfig.js
@@ -1,21 +1,25 @@
-const primary_color = '#FFDB01';
-
+const primary_color = '#54B880';
+const primary_color_light = '#54B880';
+const primary_color_dark = '#0D8549';
+const secondary_color = "#b88054";
 const base_color = '#fffff0';
 
 const heading_color = '#302b28';
 
-const fontstack_heading = ['Special Elite', 'Cursive'];
+const fontstack_sans_serif = ['Montserrat', 'sans-serif'];
 
 tailwind.config = {
     theme: {
         extend: {
             colors: {
                 'primary': primary_color,
+                'primary-dark': primary_color_dark,
+                'secondary': secondary_color,
                 'heading': heading_color,
                 'base': base_color,
             },
             fontFamily: {
-                'heading': fontstack_heading,
+                'sans-serif': fontstack_sans_serif,
             }
         }
     }
