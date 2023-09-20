b5b5ed5ea0da7001619bf0f7539912f29f7824f3
Teun Kluiving <thkluiving@st.noorderpoort.nl>
2023-09-20 10:56:58 +0200

    ok

--- js/ingredients.js
+++ js/ingredients.js
@@ -3,7 +3,7 @@ const infobox = document.querySelector('#info_box')
 const div = document.querySelector('#ingredients');
 const image_folder = "./media/dd-fotos/";
 
-console.log(infobox);
+
 
 async function fetchIngredients() {
     const res = await fetch('./db/ingredients.json');
@@ -16,23 +16,40 @@ async function createIngredients() {
 
     const ingredients = await fetchIngredients();
 
-    console.log(ingredients);
-
-    ingredients.forEach(ingredient => {
+    ingredients.forEach((ingredient, key) => {
         const ingredientDiv = document.createElement('div');
-        ingredientDiv.classList.add('w-1/4', 'p-1', 'lg:p-4' ,'hover:shadow-xl', 'transition-all', 'duration-300', 'hover:cursor-pointer');
-        ingredientDiv.innerHTML = `<img src=\"${image_folder + ingredient.image_url}\" class=\"w-full object-cover aspect-square max-w-48 hover:scale-110 transition-all duration-300 hover:shadow-xl\"><img/>`;
+        ingredientDiv.classList.add('ingredient', 'grayscale', 'w-1/4', 'lg:w-1/5', 'p-1', 'lg:p-4' ,'hover:shadow-xl', 'transition-all', 'duration-300', 'hover:cursor-pointer');
+        ingredientDiv.innerHTML = `<img src=\"${image_folder + ingredient.image_url}\" class=\"w-full object-cover aspect-square max-w-48 lg:hover:scale-110 transition-all duration-300 \"><img/>`;
+
+        if(key == 0) {
+            fillInfobox(ingredientDiv, ingredient.title, ingredient.text);
+        }
+
 
         ingredientDiv.addEventListener('click', () => {
-            infobox.querySelector('h2').innerHTML = ingredient.title;
-            infobox.querySelector('p').innerHTML = ingredient.text;
-            console.log(infobox.querySelector('h2'));
+            fillInfobox(ingredientDiv, ingredient.title, ingredient.text);
+            div.scrollIntoView();
         });
-
         div.append(ingredientDiv);
     });
 
 
+    
+}
+
+function fillInfobox(div, title, text) {
+    infobox.querySelector('h2').innerHTML = title;
+    infobox.querySelector('p').innerHTML = text;
+    clearStyle();
+    div.classList.add('scale-110', 'rounded', 'grayscale-0');
+}
+
+function clearStyle() {
+    const ingredientDivs = document.querySelectorAll('.ingredient');
+    ingredientDivs.forEach(div => {
+        div.classList.remove('scale-110', 'grayscale-0');
+    });
 }
 
 createIngredients();
+
